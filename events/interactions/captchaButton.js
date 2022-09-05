6a269e84f71e32234a5d8568e2f7bde0cc398c51
⭐ Nicollas <niclw.contact@gmail.com>
2022-09-05 19:18:10 -0300

    Update captchaButton.js

--- events/interactions/captchaButton.js
+++ events/interactions/captchaButton.js
@@ -2,14 +2,17 @@ const { EmbedBuilder } = require("discord.js");
 
 module.exports = {
   name: "interactionCreate",
-  async execute(interaction, client) {
+  async execute(interaction) {
     const { customId, guild } = interaction;
     if (customId == "captcha") {
-      const verified = guild.roles.cache.get("1016426014030708828");
+      const verified = guild.roles.cache.get("");
       if (interaction.member.roles.cache.has(verified.id)) {
-        interaction.reply({ content: "**OPSS!!** It looks like you're already verified.", ephemeral: true })
+        interaction.reply({
+          content: "**OPSS!!** It looks like you're already verified.",
+          ephemeral: true,
+        });
       } else {
-      interaction.member.roles.add(verified);
+        interaction.member.roles.add(verified);
       }
     }
   },
